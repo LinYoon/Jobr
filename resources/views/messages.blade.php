7e7476de7c9174fc354847fed2ed74c7c7cc364e
LinYoon <matej.vrankar95@gmail.com>
2018-01-12 19:59:09 +0100

    changed ui for messages

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -3,10 +3,24 @@
 @section('content')
 <div class="container">
   <div class="row">
-      <div class="col-md-8 col-md-offset-2">
-        <a href="{{route('messages')}}">Vsa sporočila</a>
-        <a href="{{route('messages.filter', 'poslana')}}">Poslana sporočila</a>
-        <a href="{{route('messages.filter', 'prejeta')}}">Prejeta sporočila</a>
+      <div class="col-md-8 col-md-offset-2 pt-30">
+
+        <nav class="navbar navbar-default navbar-custom">
+          <div class="container-fluid">
+            <ul class="nav navbar-nav">
+
+              <li class="{{ Request::is('sporocila') ? 'active' : '' }}">
+                <a href="{{route('messages')}}">Vsa sporočila</a>
+              </li>
+              <li class="{{ Request::is('sporocila/poslana') ? 'active' : '' }}">
+                <a href="{{route('messages.filter', 'poslana')}}">Poslana sporočila</a>
+              </li>
+              <li class="{{ Request::is('sporocila/prejeta') ? 'active' : '' }}">
+                <a href="{{route('messages.filter', 'prejeta')}}">Prejeta sporočila</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
 
         @if(sizeof($messages) > 0)
           @foreach($messages as $msg)
